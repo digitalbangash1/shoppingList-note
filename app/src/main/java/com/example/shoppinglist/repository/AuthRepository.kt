55b8cf77d3d7a896e3c4e04c405eff28fd2f4fe5
Muhammad Bangash <s092512@student.dtu.dk>
2022-11-23 08:33:11 +0100

    firebase auth login and logn viewmodel

---
+++ app/src/main/java/com/example/shoppinglist/repository/AuthRepository.kt
@@ -0,0 +1,61 @@
+package com.example.shoppinglist.repository
+
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class AuthRepository @Inject constructor() {
+    val currentUser: FirebaseUser? = Firebase.auth.currentUser
+
+    fun hasUser(): Boolean = Firebase.auth.currentUser != null
+
+    fun getUserId(): String = Firebase.auth.currentUser?.uid.orEmpty()
+
+
+
+
+    suspend fun createUser(
+        email: String,
+        password: String,
+        onComplete: (Boolean) -> Unit
+    ) = withContext(Dispatchers.IO) {
+
+        Firebase.auth
+            .createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    onComplete.invoke(true)
+                } else {
+                    onComplete.invoke(false)
+                }
+            }.await()
+    }
+
+    suspend fun login(
+        email: String,
+        password: String,
+        onComplete: (Boolean) -> Unit
+    ) = withContext(Dispatchers.IO) {
+
+        Firebase.auth
+            .signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    onComplete.invoke(true)
+                } else {
+                    onComplete.invoke(false)
+                }
+            }.await()
+    }
+
+    suspend fun logout() = withContext(Dispatchers.IO) {
+        Firebase.auth.signOut()
+
+
+    }
+
+}
